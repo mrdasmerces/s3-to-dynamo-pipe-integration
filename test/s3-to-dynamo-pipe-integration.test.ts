ce5e1e6f471c85c944bee9fe979d4e3cfd275453
matheusdasmerces <matheusdmerces@gmail.com>
2023-10-19 11:45:31 +0200

    chore: update tests

--- test/s3-to-dynamo-pipe-integration.test.ts
+++ test/s3-to-dynamo-pipe-integration.test.ts
@@ -1,17 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as S3ToDynamoPipeIntegration from '../lib/s3-to-dynamo-pipe-integration-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template, Match } from "aws-cdk-lib/assertions";
+import * as S3ToDynamoPipeIntegration from "../lib/s3-to-dynamo-pipe-integration-stack";
 
-test('SQS Queue and SNS Topic Created', () => {
+test("SQS Queue and SNS Topic Created", () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new S3ToDynamoPipeIntegration.S3ToDynamoPipeIntegrationStack(app, 'MyTestStack');
+  const stack = new S3ToDynamoPipeIntegration.S3ToDynamoPipeIntegrationStack(
+    app,
+    "MyTestStack"
+  );
   // THEN
 
   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
+  template.hasResourceProperties("AWS::SQS::Queue", {
+    ReceiveMessageWaitTimeSeconds: 20,
   });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+  template.resourceCountIs("AWS::S3::Bucket", 1);
 });
